c987cc0e8b396d854400516041a530ede5ba8c53
camilius13 <pcamilo743@gmail.com>
2026-04-24 01:09:29 -0500

    implementación del imageTraking para conectarse al spaceShared

---
+++ CardBattleApp/AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: CardBattleApp
+- Unity version: Unity 6000.3.6f1
+- Active game object:
+  - Name: ClientButton
+  - Tag: Untagged
+  - Layer: UI
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- CardBattleApp/Assets/Scripts/Multiplayer/IColocalizationRoom.cs
+++ CardBattleApp/Assets/Scripts/Multiplayer/IColocalizationRoom.cs
@@ -1,16 +1,9 @@
-using UnityEngine;
 
-public class IColocalizationRoom : MonoBehaviour
+
+public interface IColocalizationRoom 
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void SharedSpaceStartTracking();
+    public void CreateRoomOptions();
+    public void StartHost();
+    public void StartClient();
 }
